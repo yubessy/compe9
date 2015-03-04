5a7c9b7e210efb3f221ac13d1837e6ea7bc592b3
yubessy <yubessy0@gmail.com>
2015-03-04 23:28:27 +0900

    整理

--- predict.py
+++ predict.py
@@ -7,7 +7,6 @@ import json
 
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import Lasso
-from sklearn.preprocessing import normalize, scale
 
 
 DATA_DIR = 'data/'
@@ -15,7 +14,7 @@ LABELED_TRAIN_VEC_JSON = DATA_DIR + 'labeled_train_vec.json'
 LABELED_TEST_VEC_JSON = DATA_DIR + 'labeled_test_vec.json'
 
 
-def main():
+def load():
     with open(LABELED_TRAIN_VEC_JSON) as labeled_train_vec_file:
         labeled_train_vecs = json.loads(labeled_train_vec_file.read())
 
@@ -32,11 +31,30 @@ def main():
     train_x = dv.transform((d for d in labeled_train_vecs))
     test_x = dv.transform((d for d in labeled_test_vecs))
 
+    return dict(
+        train_i=train_i,
+        train_y=train_y,
+        test_i=test_i,
+        train_x=train_x,
+        test_x=test_x)
+
+
+def predict(train_x, train_y, test_x):
     reg = Lasso()
     reg.fit(train_x, train_y)
     test_y = reg.predict(test_x)
+    return test_y
+
+
+def output(test_i, test_y):
     for i, y in zip(test_i, test_y):
         print('{},{}'.format(i, int(y)))
+
+
+def main():
+    data = load()
+    test_y = predict(data['train_x'], data['train_y'], data['test_x'])
+    output(data['test_i'], test_y)
 
 
 if __name__ == '__main__':
