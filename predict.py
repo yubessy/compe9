2022d894e8ec59f32394a72bbeb1062150853d23
yubessy <yubessy0@gmail.com>
2015-03-04 23:01:25 +0900

    1st submission

--- predict.py
+++ predict.py
@@ -3,18 +3,40 @@
 This script is for prediction
 '''
 
-# from sklearn.regression import *
+import json
+
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import Lasso
+from sklearn.preprocessing import normalize, scale
 
 
 DATA_DIR = 'data/'
-JOINED_TRAIN_JSON = DATA_DIR + 'joined_train.json'
-JOINED_TEST_JSON = DATA_DIR + 'joined_test.json'
 LABELED_TRAIN_VEC_JSON = DATA_DIR + 'labeled_train_vec.json'
 LABELED_TEST_VEC_JSON = DATA_DIR + 'labeled_test_vec.json'
 
 
 def main():
-    pass
+    with open(LABELED_TRAIN_VEC_JSON) as labeled_train_vec_file:
+        labeled_train_vecs = json.loads(labeled_train_vec_file.read())
+
+    with open(LABELED_TEST_VEC_JSON) as labeled_test_vec_file:
+        labeled_test_vecs = json.loads(labeled_test_vec_file.read())
+
+    train_i = [d.pop('id') for d in labeled_train_vecs]
+    train_y = [d.pop('result') for d in labeled_train_vecs]
+
+    test_i = [d.pop('id') for d in labeled_test_vecs]
+
+    dv = DictVectorizer()
+    dv.fit(labeled_train_vecs + labeled_test_vecs)
+    train_x = dv.transform((d for d in labeled_train_vecs))
+    test_x = dv.transform((d for d in labeled_test_vecs))
+
+    reg = Lasso()
+    reg.fit(train_x, train_y)
+    test_y = reg.predict(test_x)
+    for i, y in zip(test_i, test_y):
+        print('{},{}'.format(i, int(y)))
 
 
 if __name__ == '__main__':
