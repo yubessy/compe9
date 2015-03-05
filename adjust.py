ca7ad595ec255d28a9765b71da77e3a4c3ea5525
yubessy <yubessy0@gmail.com>
2015-03-06 01:12:25 +0900

    6th submission

---
+++ adjust.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+'''
+This script is for adjustment.
+'''
+
+import json
+
+DATA_DIR = 'data/'
+PREDICT_CSV = DATA_DIR + 'predict.csv'
+JOINED_TRAIN_JSON = DATA_DIR + 'joined_train.json'
+JOINED_TEST_JSON = DATA_DIR + 'joined_test.json'
+
+
+def main():
+    with open(JOINED_TRAIN_JSON) as joined_train_file:
+        train = json.loads(joined_train_file.read())
+
+    with open(JOINED_TEST_JSON) as joined_test_file:
+        test = json.loads(joined_test_file.read())
+
+    with open(PREDICT_CSV) as predict_file:
+        predict = [
+            (i, int(v))
+            for i, v in (l.rstrip().split(',') for l in predict_file)]
+
+    max_by_home_team = dict()
+    min_by_home_team = dict()
+    max_by_away_team = dict()
+    min_by_away_team = dict()
+    for d in train:
+        max_by_home_team[d['home_team']] = max(
+            int(d['result']),
+            max_by_home_team.get(d['home_team'], 0))
+        min_by_home_team[d['home_team']] = min(
+            int(d['result']),
+            min_by_home_team.get(d['home_team'], int(d['capacity'])))
+        max_by_away_team[d['away_team']] = max(
+            int(d['result']),
+            max_by_away_team.get(d['away_team'], 0))
+        min_by_away_team[d['away_team']] = min(
+            int(d['result']),
+            min_by_away_team.get(d['away_team'], int(d['capacity'])))
+
+    for (i, v), d in zip(predict, test):
+        ma = (
+            max_by_home_team[d['home_team']]
+            + max_by_away_team[d['away_team']]) / 2
+        mi = (
+            min_by_home_team[d['home_team']]
+            + min_by_away_team[d['away_team']]) / 2
+        if v > ma:
+            print('hoge')
+            v = ma
+        elif v < mi:
+            print('hoge')
+            v = mi
+        print("{},{}".format(i, v))
+
+
+if __name__ == '__main__':
+    main()
--- predict.py
+++ predict.py
@@ -4,9 +4,12 @@ This script is for prediction
 '''
 
 import json
+import sys
 
+from numpy import sqrt
+from sklearn.cross_validation import cross_val_score
 from sklearn.feature_extraction import DictVectorizer
-from sklearn.linear_model import Lasso
+from sklearn.ensemble import RandomForestRegressor
 
 
 DATA_DIR = 'data/'
@@ -14,48 +17,57 @@ LABELED_TRAIN_VEC_JSON = DATA_DIR + 'labeled_train_vec.json'
 LABELED_TEST_VEC_JSON = DATA_DIR + 'labeled_test_vec.json'
 
 
-def load():
-    with open(LABELED_TRAIN_VEC_JSON) as labeled_train_vec_file:
-        labeled_train_vecs = json.loads(labeled_train_vec_file.read())
-
-    with open(LABELED_TEST_VEC_JSON) as labeled_test_vec_file:
-        labeled_test_vecs = json.loads(labeled_test_vec_file.read())
+def load_dataset(labeled_train_vecs, labeled_test_vecs, filter_features=set()):
+    if filter_features:
+        filter_features = filter_features | {'id', 'result'}
+        labeled_train_vecs = [
+            {k: v for k, v in d.items() if k.split('#')[0] in filter_features}
+            for d in labeled_train_vecs]
+        labeled_train_vecs = [
+            {k: v for k, v in d.items() if k.split('#')[0] in filter_features}
+            for d in labeled_test_vecs]
+    else:
+        labeled_train_vecs = labeled_train_vecs.copy()
+        labeled_test_vecs = labeled_test_vecs.copy()
 
     train_i = [d.pop('id') for d in labeled_train_vecs]
-    train_y = [d.pop('result') for d in labeled_train_vecs]
-
     test_i = [d.pop('id') for d in labeled_test_vecs]
+
+    train_y = [d.pop('result') for d in labeled_train_vecs]
 
     dv = DictVectorizer()
     dv.fit(labeled_train_vecs + labeled_test_vecs)
     train_x = dv.transform((d for d in labeled_train_vecs))
     test_x = dv.transform((d for d in labeled_test_vecs))
 
-    return dict(
-        train_i=train_i,
-        train_y=train_y,
-        test_i=test_i,
-        train_x=train_x,
-        test_x=test_x)
+    return train_i, test_i, train_x, test_x, train_y
 
 
-def predict(train_x, train_y, test_x):
-    reg = Lasso()
-    reg.fit(train_x, train_y)
-    test_y = reg.predict(test_x)
-    return test_y
+def main(mode):
+    with open(LABELED_TRAIN_VEC_JSON) as labeled_train_vec_file:
+        labeled_train_vecs = json.loads(labeled_train_vec_file.read())
 
+    with open(LABELED_TEST_VEC_JSON) as labeled_test_vec_file:
+        labeled_test_vecs = json.loads(labeled_test_vec_file.read())
 
-def output(test_i, test_y):
-    for i, y in zip(test_i, test_y):
-        print('{},{}'.format(i, int(y)))
+    train_i, test_i, train_x, test_x, train_y = load_dataset(
+        labeled_train_vecs, labeled_test_vecs)
 
+    reg = RandomForestRegressor()
 
-def main():
-    data = load()
-    test_y = predict(data['train_x'], data['train_y'], data['test_x'])
-    output(data['test_i'], test_y)
+    if mode == '-o':
+        reg.fit(train_x.toarray(), train_y)
+        test_y = reg.predict(test_x.toarray())
+        for i, y in zip(test_i, test_y):
+            print('{},{}'.format(i, int(y)))
+
+    elif mode == '-c':
+        mses = cross_val_score(
+            reg, train_x.toarray(), train_y,
+            cv=5, scoring='mean_squared_error')
+        rmses = sqrt(-mses)
+        print("RMSE: {:.2f} +/- {:.2f}".format(rmses.mean(), rmses.std() * 2))
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
